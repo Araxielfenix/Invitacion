f5474bfa6c212777f7751954365fe749d0a92132
Araxielfenix <araxielfenix@outlook.com>
2025-09-25 18:54:36 -0600

    Update vite.config.ts
    Se agregó la ruta del repositorio al url base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin(), tailwindcss()],
+  base: "/invitacion",
   server: {
     port: 3000,
   },

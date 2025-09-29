c02441b45da4d04b3f8777c952bebd9848ceb1ef
araxielfenix <araxielfenix@outlook.com>
2025-09-29 11:58:33 -0600

    Se ha cambiado la hora del evento

--- src/fechaHora.tsx
+++ src/fechaHora.tsx
@@ -3,7 +3,8 @@ import fechaYHora from './media/fechaHora.png';
 const fechaHora = () => {
     return (
         <div id="panelFechaHora">
-            <div class="flex flex-col items-center shrink" >
+            <div class="grid grid-cols-1 justify-items-center" >
+                <p></p>
                 <img class='lg:scale-50 md: w-screen' src={fechaYHora} alt="Fecha y Hora" />
             </div>
         </div>

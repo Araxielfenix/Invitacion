19540690a9676ea53cb12df74d0e58c973b4a934
araxielfenix <araxielfenix@outlook.com>
2025-09-29 21:28:54 -0600

    Se cambio el iframe para coincidir con la ubicación

--- src/ubicacion.tsx
+++ src/ubicacion.tsx
@@ -2,7 +2,7 @@ const direccion = () => {
   return (
     <div id="panelDireccion">
       <div class="embed-map-responsive">
-        <a href="https://maps.app.goo.gl/Su7BNWiofmHhW1jd7">
+        <a href="https://maps.app.goo.gl/38Z9bV3B1qm3gzTEA">
           <div
             class="
                 flex items-center justify-center 
@@ -10,14 +10,14 @@ const direccion = () => {
                 max-w-lg mx-auto aspect-video lg:scale-y-120 
                 overflow-hidden 
                 [clip-path:polygon(50%_0%,70%_50%,50%_100%,30%_50%)]
-                hover:animate-pulse border-4 border-b-blue-950
+                hover:animate-pulse
             "
           >
             <iframe
-              class="w-full h-full border-none justify-items-center"
-              src="https://maps.google.com/maps?width=600&height=400&hl=en&q=Bosque%20de%20San%20juan%20de%20Arag%C3%B3n&t=&z=14&ie=UTF8&iwloc=B&output=embed"
-            ></iframe>
+              class="w-screen h-full border-none justify-items-center"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2993.994315767661!2d-99.07934886643783!3d19.458477306755217!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fbd511dfda3b%3A0x266c54c1b8d4aa02!2s%C3%81rea%20de%20Palapas%20-%20Bosque%20de%20San%20Juan%20de%20Arag%C3%B3n!5e0!3m2!1sen!2sus!4v1759202260265!5m2!1sen!2sus" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
+
         </a>
       </div>
     </div>
